ea6f5fed68787535f35624ce8b8ca3f681ce4c22
jfaldanam <jfaldanam@gmail.com>
2019-04-04 09:10:37 +0200

    Creation of pending friendships from search

---
+++ inTouch-war/src/java/inTouchServlets/addFriendServlet.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inTouchServlets;
+
+import inTouch.ejb.PendingFriendshipFacade;
+import inTouch.ejb.UserFacade;
+import inTouch.entity.PendingFriendship;
+import inTouch.entity.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author jfaldanam
+ */
+@WebServlet(name = "addFriendServlet", urlPatterns = {"/addFriend"})
+public class addFriendServlet extends HttpServlet {
+
+    @EJB
+    private UserFacade userFacade;
+    @EJB
+    private PendingFriendshipFacade pendingFriendshipFacade;
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+        int userId = (Integer)session.getAttribute("userId");
+        User sender = this.userFacade.find(userId);
+        
+        String addUserStr = request.getParameter("addUserId");
+        int addUserId = -1;       
+        try {
+            addUserId = Integer.parseInt(addUserStr);
+        } catch(NumberFormatException e) {
+            //TODO
+        }
+        User receiver = this.userFacade.find(addUserId);
+        
+        PendingFriendship pending = new PendingFriendship(new Random().nextInt());
+        pending.setSender(sender);
+        pending.setReceiver(receiver);
+        
+        this.pendingFriendshipFacade.create(pending);
+        
+        RequestDispatcher rd = request.getRequestDispatcher("search");
+        rd.forward(request, response);
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- inTouch-war/web/search.jsp
+++ inTouch-war/web/search.jsp
@@ -42,8 +42,11 @@
                 %>
                 <tr>
                     <td>
-                        <center><%=user.getUsername()%> <input type="submit" value="A&ntilde;adir amigo"></center><br/>
-                        TODO: Mostrar ultimo post. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <form action="addFriend" method="get" name="addFriend">
+                            <input type="hidden" name="addUserId" value="<%=user.getId()%>">
+                            <center><%=user.getUsername()%> <input type="submit" value="A&ntilde;adir amigo"></center><br/>
+                            TODO: Mostrar ultimo post. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </form>
                     </td>
                 </tr>
                 <%
